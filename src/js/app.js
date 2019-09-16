154e150f78df887514684ea1977339c42f053354
Alfonso Hernandez <alfonso.hernandez@naobatec.com>
2019-09-16 15:32:32 -0500

    se agrega css

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,5 @@
 import { message } from './messages'
 
+require('../css/app.css')
+
 document.getElementById('boton').addEventListener('click', message);
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,33 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
     mode: 'development',
-    entry: path.join(__dirname, '/', 'src/js/myfile.js'),
+    entry: path.join(__dirname, '/', 'src/js/app.js'),
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'output123.js'
+        filename: 'app.js'
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: 'index.html'
+        }),
+
+        new MiniCssExtractPlugin({
+            filename: 'output.css'
         })
-    ]
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [{
+                    loader: MiniCssExtractPlugin.loader,
+                }, 'css-loader'
+                ]
+            }
+        ]
+    }
 }
